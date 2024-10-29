00c01aadd450fab3ff9c5ed92ca0fec1a8e8b952
elysableu <elysabward@gmail.com>
2024-10-29 15:57:32 -0600

    Tests: add empty it blocks

---
+++ spec/turn_spec.rb
@@ -0,0 +1,45 @@
+require './spec/spec_helper'
+
+RSpec.describe Turn do
+  describe "#initialize" do
+    it "exists" do
+
+    end
+
+    it "can display boards" do
+
+    end
+  end
+
+  describe "#turn_shot" do
+    it "can have player fire a shot" do
+
+    end
+
+    it "can have computer fire a shot" do
+
+    end
+  end
+
+  describe "#report_results" do
+    it "can update coordinate status" do
+
+    end
+
+    it "can report a hit" do
+
+    end
+
+    it "can report a miss" do
+
+    end
+
+    it "can repost a ship sunk" do
+
+    end
+
+    it "can report if coordinate has already been fired upon" do
+      
+    end
+  end
+end
