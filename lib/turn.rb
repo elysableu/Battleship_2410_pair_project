0b6797230a66228766ec557c166268d962393203
elysableu <elysabward@gmail.com>
2024-10-30 14:16:48 -0600

    Add computer_fire_upon behavior and random_coordinate helper method

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,4 +22,19 @@ class Turn
     end
   end
 
+  def computer_fire_shot 
+    computer_fired = random_coordinate
+    if @player_board.cells[computer_fired].fired_upon? == false
+      @player_board.cells[computer_fired].fire_upon
+      return true
+    else
+      return false
+    end
+  end
+
+  def random_coordinate
+    @player_board.cells.keys.sample
+  end
+
+  
 end
