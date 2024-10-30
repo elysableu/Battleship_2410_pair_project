423fe466f93958ac3ad3fc69c6d29cfc725a925e
elysableu <elysabward@gmail.com>
2024-10-30 15:44:21 -0600

    Add fire_shots behavior

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,16 @@
 class Turn
-  attr_reader :player_fired, :player_board, :computer_board
+  attr_reader :player_fired, 
+              :player_board, 
+              :computer_board, 
+              :player_result, 
+              :computer_result
+
   def initialize(player_fired, player_board, computer_board)
     @player_fired = player_fired
     @player_board = player_board
     @computer_board = computer_board
+    @player_result = ""
+    @computer_result = ""
   end
 
   def display
@@ -16,8 +23,21 @@ class Turn
   def player_fire_shot
     if @computer_board.cells[@player_fired].fired_upon? == false
       @computer_board.cells[@player_fired].fire_upon
-      return true
+      if @computer_board.cells[@player_fired].empty? == false
+        ship = @computer_board.cells[@player_fired].ship
+        if ship.health != 0
+          @player_result = "Hit"
+          return true
+        else
+          @player_result = "Sunk"
+          return true
+        end
+      else
+        @player_result = "Miss"
+        return true
+      end
     else 
+      @player_result = "Player Invalid"
       return false
     end
   end
@@ -26,8 +46,21 @@ class Turn
     computer_fired = random_coordinate
     if @player_board.cells[computer_fired].fired_upon? == false
       @player_board.cells[computer_fired].fire_upon
-      return true
-    else
+      if @player_board.cells[computer_fired].empty? == false
+        ship = @player_board.cells[computer_fired].ship
+        if ship.health != 0
+          @computer_result = "Hit"
+          return true
+        else
+          @computer_result = "Sunk"
+          return true
+        end
+      else
+        @computer_result = "Miss"
+        return true
+      end
+    else 
+      @computer_result = "Computer Invalid"
       return false
     end
   end
@@ -36,5 +69,8 @@ class Turn
     @player_board.cells.keys.sample
   end
 
-  
+  def fire_shots
+    player_fire_shot
+    computer_fire_shot
+  end
 end
