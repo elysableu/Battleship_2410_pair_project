a043bd162860a706c77c78c9e3ffb781916882e0
elysableu <elysabward@gmail.com>
2024-10-30 16:40:50 -0600

    Add tests to setup

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,19 +1,18 @@
 require './spec/spec_helper'
 
 RSpec.describe Game do
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @computer_board = Board.new
+    @player_board = Board.new
+    @turn_1 = Turn.new("D3", @player_board, @computer_board)
+    @turn_2 = Turn.new("D2", @player_board, @computer_board)
+    @turn_3 = Turn.new("D4", @player_board, @computer_board)
+    @turn_4 = Turn.new("D2", @player_board, @computer_board)
+    @game = Game.new 
+  end
   describe "#initialize" do
-    before(:each) do
-      @cruiser = Ship.new("Cruiser", 3)
-      @submarine = Ship.new("Submarine", 2)
-      @computer_board = Board.new
-      @player_board = Board.new
-      @turn_1 = Turn.new("D3", @player_board, @computer_board)
-      @turn_2 = Turn.new("D2", @player_board, @computer_board)
-      @turn_3 = Turn.new("D4", @player_board, @computer_board)
-      @turn_4 = Turn.new("D2", @player_board, @computer_board)
-      @game = Game.new 
-    end
-
     it "exists" do
       expect(@game).to be_a(Game)
     end
@@ -32,6 +31,19 @@ RSpec.describe Game do
       expect(@game.setup(@computer_board, 
                         computer_cruiser_placement, 
                         computer_submarine_placement)).to be true
+    end
+
+    it "can update computer board with setup" do
+      computer_cruiser_placement = ["A4", "B4", "C4"]
+      computer_submarine_placement = ["D3", "D4"]
+      setup_computer_board =  "  1 2 3 4 \n" + 
+                              "A . . . S \n" + 
+                              "B . . . S \n" + 
+                              "C . . . S \n" + 
+                              "D . . S S \n"
+
+      @game.setup(@computer_board, computer_cruiser_placement, computer_submarine_placement)
+      expect(@computer_board.render(true)).to eq(setup_computer_board)
     end
 
     it "can create random valid placements for computer ships" do
@@ -75,6 +87,19 @@ RSpec.describe Game do
                         player_submarine_placement)
       
       expect(@game.message).to eq("That's an invalid placement! Try again: \n> ")                  
+    end
+
+    it "can update player board when setup" do
+      player_cruiser_placement = ["B1", "B2", "B3"]
+      player_submarine_placement = ["A1", "A2"]
+      setup_player_board =  "  1 2 3 4 \n" + 
+                              "A S S . . \n" + 
+                              "B S S S . \n" + 
+                              "C . . . . \n" + 
+                              "D . . . . \n"
+
+      @game.setup(@player_board, player_cruiser_placement, player_submarine_placement)
+      expect(@player_board.render(true)).to eq(setup_player_board)
     end
   end
 end
